c790ce4f2abf4166ebfdaf0cbc400e2d06906d6b
Victor Zhong <victor@victorzhong.com>
2017-10-17 23:44:46 +0000

    enable travis

---
+++ test/check.py
@@ -0,0 +1,30 @@
+import json
+from tqdm import tqdm
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from lib.query import Query
+from lib.dbengine import DBEngine
+
+
+if __name__ == '__main__':
+    for split in ['train', 'dev', 'test']:
+        print('checking {}'.format(split))
+        engine = DBEngine('data/{}.db'.format(split))
+        n_lines = 0
+        with open('data/{}.jsonl'.format(split)) as f:
+            for l in f:
+                n_lines += 1
+        with open('data/{}.jsonl'.format(split)) as f:
+            for l in tqdm(f, total=n_lines):
+                d = json.loads(l)
+                query = Query.from_dict(d['sql'])
+    
+                # make sure it's executable
+                result = engine.execute_query(d['table_id'], query)
+                if result:
+                    for a, b, c in d['sql']['conds']:
+                        if str(c).lower() not in d['question'].lower():
+                            raise Exception('Could not find conditon {} in question {} for query {}'.format(c, d['question'], query))
+                else:
+                    raise Exception('Query {} did not execute to a valid result'.format(query))
